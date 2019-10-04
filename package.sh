2b81b88a63b77db6b77bf84edc19f43d2113a1d9
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2019-10-04 23:20:03 +0200

    Add manifest.json

--- package.sh
+++ package.sh
@@ -2,7 +2,7 @@
 
 npm install --production
 
-shasum --algorithm 256 package.json lib/*.js LICENSE > SHA256SUMS
+shasum --algorithm 256 package.json manifest.json lib/*.js LICENSE README.md > SHA256SUMS
 find node_modules -type f -exec shasum --algorithm 256 {} \; >> SHA256SUMS
 
 TARFILE=`npm pack`
