b8db49a6cf10fe6e6452ff0b743d56097e6a0432
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2020-07-10 00:45:32 +0200

    Add cross-compile build

--- package.sh
+++ package.sh
@@ -1,17 +1,35 @@
-#!/bin/bash
+#!/bin/bash -e
 
+# Setup environment for building inside Dockerized toolchain
+export NVM_DIR="${HOME}/.nvm"
+[ -s "${NVM_DIR}/nvm.sh" ] && source "${NVM_DIR}/nvm.sh"
+[ $(id -u) = 0 ] && umask 0
+
+npm ci
+npm run build
 rm -rf node_modules
-npm install --production
 
-shasum --algorithm 256 package.json manifest.json lib/*.js LICENSE README.md > SHA256SUMS
-find node_modules -type f -exec shasum --algorithm 256 {} \; >> SHA256SUMS
+if [ -z "${ADDON_ARCH}" ]; then
+  TARFILE_SUFFIX=
+else
+  NODE_VERSION="$(node --version)"
+  TARFILE_SUFFIX="-${ADDON_ARCH}-${NODE_VERSION/\.*/}"
+fi
+
+npm ci --production
+
+shasum --algorithm 256 manifest.json package.json lib/*.js LICENSE README.md > SHA256SUMS
+
+find node_modules \( -type f -o -type l \) -exec shasum --algorithm 256 {} \; >> SHA256SUMS
 
 TARFILE=`npm pack`
+
 tar xzf ${TARFILE}
+rm ${TARFILE}
+TARFILE_ARCH="${TARFILE/.tgz/${TARFILE_SUFFIX}.tgz}"
 cp -r node_modules ./package
-tar czf ${TARFILE} package
+tar czf ${TARFILE_ARCH} package
 
-shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
+shasum --algorithm 256 ${TARFILE_ARCH} > ${TARFILE_ARCH}.sha256sum
 
-rm SHA256SUMS
-rm -rf package
+rm -rf SHA256SUMS package
