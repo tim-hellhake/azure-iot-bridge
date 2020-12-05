9fc4840b8efe971a0af2d0a0eae3cc91becdfb43
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2020-12-05 17:40:51 +0100

    Switch to webthingsio toolchain

--- build.sh
+++ build.sh
@@ -50,7 +50,7 @@ function build_native() {
 }
 
 function build_cross_compiled() {
-  docker run --rm -t -v $PWD:/build mozillaiot/toolchain-${ADDON_ARCH}-${LANGUAGE_NAME}-${LANGUAGE_VERSION} bash -c "cd /build; ADDON_ARCH=${ADDON_ARCH} ./package.sh"
+  docker run --rm -t -v $PWD:/build webthingsio/toolchain-${ADDON_ARCH}-${LANGUAGE_NAME}-${LANGUAGE_VERSION} bash -c "cd /build; ADDON_ARCH=${ADDON_ARCH} ./package.sh"
 }
 
 case "${ADDON_ARCH}" in
