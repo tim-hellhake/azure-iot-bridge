7dbd726aa6b29e6c3024198902aef1832b393dca
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2021-03-24 23:47:24 +0100

    Show error popup if config is incomplete (#7)

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,24 @@
 import {AddonManagerProxy} from 'gateway-addon';
 import {AzureIotBridge, Manifest} from './azure-iot-bridge';
 
-export = function(addonManager: AddonManagerProxy, manifest: Manifest): void {
+export = function(addonManager: AddonManagerProxy,
+                  manifest: Manifest,
+                  errorCallback:
+                  // eslint-disable-next-line no-unused-vars
+                  (packageName: string, error: string) => void): void {
+  const {
+    hubConnectionString,
+    accessToken,
+  } = manifest.moziot.config;
+
+  if (!hubConnectionString) {
+    errorCallback(manifest.name, 'No hub connection string configured');
+    return;
+  }
+
+  if (!accessToken) {
+    errorCallback(manifest.name, 'No access token configured');
+  }
+
   new AzureIotBridge(addonManager, manifest);
 };
